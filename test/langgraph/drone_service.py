0bb1be8cf84cd96e7b1e58d466eb385c9983a003
biegehydra <connorhallman29@gmail.com>
2025-10-24 10:13:34 -0400

    Dont check in air for arm

--- test/langgraph/drone_service.py
+++ test/langgraph/drone_service.py
@@ -64,9 +64,6 @@ class DroneService:
         if not self.is_connected:
             mission_log("--- Drone not connected. Cannot arm.")
             return False
-        if await is_in_air():
-            mission_log("--- Drone is in air. Already armed.")
-            return True
         mission_log("--- Arming drone...")
         await self.drone.action.arm()
         return True
